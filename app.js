de598b6cdaebde83f7d9a06704dbd9fb9776757c
ritikgupt <ritikgupta89369@gmail.com>
2020-04-09 13:09:21 +0530

    backend integration started

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ var g=require("express-sanitizer");
 var passport=require("passport");
 var e=require("passport-local");
 var h=require("passport-local-mongoose");
-var app=a();
+var app=a(); 
 // var User=require("./models/user");
-var Intern=require("./models/intern");
-var Team=require("./models/team");
 app.use(b.urlencoded({ extended: true }));
 c.connect("mongodb://localhost:27017/amz_data", { useNewUrlParser: true,useFindAndModify : false,useUnifiedTopology: true });
 app.set("view engine","ejs");
@@ -34,115 +32,108 @@ app.use(require("express-session")
 //     res.render("header")
 // })
 app.get("/",function(req,res){
-    Team.find({},function(err,teams){
-        if(err)
-        console.log("error");
-    else
-    {
-    res.render("home",{teams:teams});
-    }
+    res.render("amz");
 })
-})
-app.get("/123abcsecret",function(req,res){
-res.render("adminlogin"); 
-})
-app.get("/team",function(req,res){
-    res.render("create_team")
-})
-app.post("/team",function(req,res){
-    Team.create({ 
-        name:req.body.team.name,
-        designation:req.body.team.designation,
-        email:req.body.team.email,
-        linkedin:req.body.team.linkedin,
-        github:req.body.team.github,
-        number:req.body.team.number,
-        image:req.body.team.image,
-        type:req.body.team.type,
-    }) 
-    res.redirect("/");
-})
-app.get("/ateam",function(req,res){
-    Team.find({},function(err,teams){
-        if(err)
-        console.log("error");
-    else
-    {
-       res.render("ateam",{teams:teams})
-    }
-})
-})
-app.get("/tteam",function(req,res){
-    Team.find({},function(err,teams){
-        if(err)
-        console.log("error");
-    else
-    {
-       res.render("tteam",{teams:teams})
-    }
-})
+// app.get("/123abcsecret",function(req,res){
+// res.render("adminlogin"); 
+// })
+// app.get("/team",function(req,res){
+//     res.render("create_team")
+// })
+// app.post("/team",function(req,res){
+//     Team.create({ 
+//         name:req.body.team.name,
+//         designation:req.body.team.designation,
+//         email:req.body.team.email,
+//         linkedin:req.body.team.linkedin,
+//         github:req.body.team.github,
+//         number:req.body.team.number,
+//         image:req.body.team.image,
+//         type:req.body.team.type,
+//     }) 
+//     res.redirect("/");
+// })
+// app.get("/ateam",function(req,res){
+//     Team.find({},function(err,teams){
+//         if(err)
+//         console.log("error");
+//     else
+//     {
+//        res.render("ateam",{teams:teams})
+//     }
+// })
+// })
+// app.get("/tteam",function(req,res){
+//     Team.find({},function(err,teams){
+//         if(err)
+//         console.log("error");
+//     else
+//     {
+//        res.render("tteam",{teams:teams})
+//     }
+// })
 
-})
-app.get("/register_course",function(req,res){
-    res.render("register_course")
-})
-app.get("/intern",function(req,res){
-    res.render("intern");
-})
-app.post("/intern",function(req,res){
-    Intern.create({
-        name:req.body.intern.name,
-        email:req.body.intern.email,
-        phone:req.body.intern.phone,
-        college:req.body.intern.college,
-        branch:req.body.intern.branch,
-        course:req.body.intern.course,
-        venue:req.body.intern.venue,
-        referal:req.body.intern.referal
-    })
-    res.redirect("/");
-})
-app.get("/:id",function(req,res){
-    Team.findById(req.params.id,function(err,foundTeam){
-        if(err){
-            res.redirect("/");
-        }
-        else{
-            res.render("show",{team:foundTeam});
-        }
-    })
+// })
+// app.get("/register_course",function(req,res){
+//     res.render("register_course")
+// })
+// app.get("/intern",function(req,res){
+//     res.render("intern");
+// })
+// app.post("/intern",function(req,res){
+//     Intern.create({
+//         name:req.body.intern.name,
+//         email:req.body.intern.email,
+//         phone:req.body.intern.phone,
+//         college:req.body.intern.college,
+//         branch:req.body.intern.branch,
+//         course:req.body.intern.course,
+//         venue:req.body.intern.venue,
+//         referal:req.body.intern.referal
+//     })
+//     res.redirect("/");
+// })
+// app.get("/:id",function(req,res){
+//     Team.findById(req.params.id,function(err,foundTeam){
+//         if(err){
+//             res.redirect("/");
+//         }
+//         else{
+//             res.render("show",{team:foundTeam});
+//         }
+//     })
 
-})
-app.delete("/:id",function(req,res){
-    Team.findByIdAndRemove(req.params.id,function(err){
-        if(err){
-            res.redirect("/ateam")
-        }
-        else{
-            res.redirect("/")
-        }
-    })
-})
-app.get("/:id/edit",function(req,res){
-    Team.findById(req.params.id,function(err,foundTeam){
-        if(err){
-            console.log("Error");
-        }
-        else{
-            res.render("edit_team",{team:foundTeam})
-        }
-    })
-})    
-app.post("/:id",function(req,res){
-    Team.findByIdAndUpdate(req.params.id,req.body.team,function(err,updatedTeam){
-        if(err){
-            res.redirect("/");
-        }
-        else{
-            res.redirect("/"+req.params.id);
-        }
-    })
-})
+// })
+// app.delete("/:id",function(req,res){
+//     Team.findByIdAndRemove(req.params.id,function(err){
+//         if(err){
+//             res.redirect("/ateam")
+//         }
+//         else{
+//             res.redirect("/")
+//         }
+//     })
+// })
+// app.get("/:id/edit",function(req,res){
+//     Team.findById(req.params.id,function(err,foundTeam){
+//         if(err){
+//             console.log("Error");
+//         }
+//         else{
+//             res.render("edit_team",{team:foundTeam})
+//         }
+//     })
+// })    
+// app.post("/:id",function(req,res){
+//     Team.findByIdAndUpdate(req.params.id,req.body.team,function(err,updatedTeam){
+//         if(err){
+//             res.redirect("/");
+//         }
+//         else{
+//             res.redirect("/"+req.params.id);
+//         }
+//     })
+// })
 app.listen("3000",function(){
     console.log("Server has started.");
 });
