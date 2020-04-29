19ad7ca3ff48bdc173a81ca3d6cb3d0af0c5ed08
ritikgupt <ritikgupta89369@gmail.com>
2020-04-29 17:10:15 +0530

    made changes in routes

--- app.js
+++ app.js
@@ -23,23 +23,11 @@ var consultRoutes=require("./routes/consult");
 var innovativeRoutes=require("./routes/innovative");
 var messageRoutes=require("./routes/message");
 var internRoutes=require("./routes/intern");
-var multer=require("multer");
-var storage=multer.diskStorage({
-  destination:function(req,file,cb){
-    cb(null,'uploads/');
-  },
-  filename:function(req,file,cb){
-    cb(null,new Date().toISOString()+file.originalname);
-  }
-});
-// var filefilter=(req,file,cb)=>{
-//   if(file.mimetype===)
-// }
-var upload=multer({storage:storage});
+var detailRoutes=require("./routes/detail");
+var dashboardRoutes=require("./routes/dashboard");
 var app=a(); 
 app.use(b.urlencoded({ extended: true }));
 var Detail=require("./models/detail");
-var Request=require("./models/request");
 var Request_staff=require("./models/request_staff");
 c.connect("mongodb://localhost:27017/amz", { useNewUrlParser: true,useFindAndModify : false,useUnifiedTopology: true });
 app.set("view engine","ejs");
@@ -79,10 +67,12 @@ app.use(consultRoutes);
 app.use(innovativeRoutes);
 app.use(messageRoutes);
 app.use(internRoutes)
+app.use(dashboardRoutes);
 //body parser only parses url encoded bodies or json bodies
 // app.get("/header",function(req,res){
 //     res.render("header")
 // })
+
 app.get("/about",function(req,res){
     res.render("about");
 })
@@ -98,44 +88,7 @@ app.get("/civil",function(req,res){
 app.get("/electrical",function(req,res){
     res.render("electrical");
 })
-app.get("/login",function(req,res){
-    res.render("login");
-})
-app.post("/login",passport.authenticate("local",{
-  successRedirect:"/dashboard",
-  failureRedirect:"/login"
-}))
-app.get("/dashboard",isLoggedIn,function(req,res){
-  console.log(req.user);
-  if(req.user.type=="Employee")
-  res.render("staff",{currentStaff:req.user});
-  else
-  res.render("aspiring",{currentStudent:req.user});
-})
-app.post("/dashboard",upload.single('request[req_file]'),(req,res,next)=>{
-  if(req.user.type=="Student")
-  {
-  Request.create(
-    {
-        desc:req.body.request.desc,
-        recep:req.body.request.recep,
-        stu_id:req.user.id,
-        req_file:req.file.path,
-        date:Date.now()
-    }
-)
-  res.redirect("/dashboard")}
-  else{
-    Request_staff.create({
-      desc:req.body.request_staff.desc,
-      credit:req.body.request_staff.credit,
-      purpose:req.body.request_staff.purpose,
-      empid:req.user.id,
-      date:req.body.request_staff.date
-    })
-res.redirect("/dashboard");
-  }
-})
+
 app.get("/logout",function(req,res){
     req.logout(); 
     res.redirect("/login");
@@ -149,27 +102,7 @@ app.get("/offline",function(req,res){
 })
 
 
-app.post("/:id/edituser",function(req,res){
- console.log(req.body.detail)
- Detail.findByIdAndUpdate(req.params.id,req.body.detail,function(err,updatedDetail){
-      if(err){
-          res.redirect("/adminhome");
-      }
-      else{
-          res.redirect("/"+req.params.id);
-      }
-  })
-})
-app.get("/:id/changeuserphoto",function(req,res){
-  Detail.findById(req.params.id,function(err,foundDetail){
-      if(err){
-          console.log("Error");
-      }
-      else{
-          res.render("changeuserphoto",{detail:foundDetail})
-      }
-  }) 
-})
+
 app.get("/newprogram",function(req,res){
   res.render("program");
 })
@@ -183,56 +116,8 @@ app.post("/newprogram",function(req,res){
   })
   res.redirect("/")
 })
-function isLoggedIn(req,res,next){
-  if(req.isAuthenticated()){
-      return next();
-  }
-  else
-  res.redirect("/login");
-}
-app.get("/newuser",function(req,res){
-  res.render("newuser");
-})
-app.post("/newuser",upload.single("detail[file]"),function(req,res){
-  console.log(req.file)
-Detail.create({
-  name:req.body.detail.name,
-  username:req.body.detail.username,
-  password:req.body.detail.password,
-  email:req.body.detail.email,
-  mobile:req.body.detail.mobile,
-  dept:req.body.detail.dept,
-  type:req.body.detail.type,
-  image:req.body.detail.image,
-  file:req.file.path,
-  college:req.body.detail.college
-})
-res.redirect("/login");
-})
-app.get("/students",function(req,res){
-  Detail.find({},function(err,details){
-    if(err)
-    console.log(err)
-    else
-    res.render("students",{details:details})
-  })
-  
-})
-app.get("/employee",function(req,res){
-  Detail.find({},function(err,details){
-    if(err)
-    console.log(err)
-    else
-    res.render("employee",{details:details})
-  })
-  
-})
 
-app.get("/logout",function(req,res){
-    req.logout();
-    console.log(req.user)
-    res.redirect("/login");
-})
+
 
 
 
@@ -265,40 +150,9 @@ detail=[]
   res.render("show",{detail:detail,request:request})
 })
 
-app.get("/:id/edituser",function(req,res){
-  Detail.findById(req.params.id,function(err,foundDetail){
-      if(err){
-          console.log("Error");
-      }
-      else{
-          res.render("edituser",{detail:foundDetail})
-      }
-  })
-})    
-app.delete("/:id/",function(req,res){
-  Detail.findByIdAndRemove(req.params.id,function(err){
-      if(err){
-          res.redirect("/")
-      }
-      else{
-          res.redirect("/students")
-      }
-  })
-})
 
-app.post("/:id/changeuserphoto",upload.single("detail[file]",{overwrite:true}),function(req,res){
-  console.log(req.file.path)
-       Detail.findByIdAndUpdate(req.params.id,{file:req.file.path},function(err,updatedDetail){
-          if(err){
-              res.redirect("/adminhome");
-          }
-          else{
-              res.redirect("/"+req.params.id);
-          }
-      })
-  })
 app.use(homeRoutes);
-
+app.use(detailRoutes);
 app.listen("3000",function(){
     console.log("Server has started.");
 });
---
+++ routes/dashboard.js
@@ -0,0 +1,70 @@
+var Request=require("../models/request");
+var Request_staff=require("../models/request_staff");
+var Detail=require("../models/detail");
+var passport=require("passport");
+var e=require("passport-local");
+var express=require("express");
+var router=express.Router();
+var multer=require("multer");
+var storage=multer.diskStorage({
+  destination:function(req,file,cb){
+    cb(null,'uploads/');
+  },
+  filename:function(req,file,cb){
+    cb(null,new Date().toISOString()+file.originalname);
+  }
+});
+// var filefilter=(req,file,cb)=>{
+//   if(file.mimetype===)
+// }
+var upload=multer({storage:storage});
+passport.use( new e(Detail.authenticate()));
+passport.serializeUser(Detail.serializeUser());
+passport.deserializeUser(Detail.deserializeUser());
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    else
+    res.redirect("/login");
+  }
+  
+router.get("/login",function(req,res){
+    res.render("login");
+})
+router.post("/login",passport.authenticate("local",{
+  successRedirect:"/dashboard",
+  failureRedirect:"/login"
+}))
+router.get("/dashboard",isLoggedIn,function(req,res){
+  console.log(req.user);
+  if(req.user.type=="Employee")
+  res.render("staff",{currentStaff:req.user});
+  else
+  res.render("aspiring",{currentStudent:req.user});
+})
+router.post("/dashboard",upload.single('request[req_file]'),(req,res,next)=>{
+  if(req.user.type=="Student")
+  {
+  Request.create(
+    {
+        desc:req.body.request.desc,
+        recep:req.body.request.recep,
+        stu_id:req.user.id,
+        req_file:req.file.path,
+        date:Date.now()
+    }
+)
+  res.redirect("/dashboard")}
+  else{
+    Request_staff.create({
+      desc:req.body.request_staff.desc,
+      credit:req.body.request_staff.credit,
+      purpose:req.body.request_staff.purpose,
+      empid:req.user.id,
+      date:req.body.request_staff.date
+    })
+res.redirect("/dashboard");
+  }
+})
+module.exports=router;
