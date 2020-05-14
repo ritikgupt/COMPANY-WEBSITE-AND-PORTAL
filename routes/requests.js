6322241c03a5fa32b1553df34ce8484d665570d4
ritikgupt <ritikgupta89369@gmail.com>
2020-05-14 15:25:52 +0530

    edit file route for requests from client side added

--- routes/requests.js
+++ routes/requests.js
@@ -1,6 +1,14 @@
 var Request = require('../models/request');
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+var cloudinary = require('cloudinary');
+cloudinary.config({
+  cloud_name: 'dzsms0nne',
+  api_key: '542159551497727',
+  api_secret: 'yRkiZK6Gf4eNNhXqvrNI9WHFKM0',
+});
 router.get('/requests', (req, res) => {
   Request.find({}, (err, requests) => {
     if (err) {
@@ -27,6 +35,30 @@ router.post('/hello/:id/edit', (req, res) => {
     } else {
       res.redirect('/requests');
     }
+  });
+});
+router.get('/hello/:id/changefile', (req, res) => {
+  Request.findById(req.params.id, (err, foundRequest) => {
+    if (err){
+      console.log('err');
+    } else {
+      res.render('changerequestfile', {request: foundRequest});
+    }
+  });
+});
+router.post('/hello/:id/changefile', upload.single('request[req_file]', {overwrite: true}), (req, res) => {
+  cloudinary.v2.uploader.upload(req.file.path, {overwrite: true}, function(err, result){
+    console.log('entered');
+    if (err){
+      console.log('err');
+    }
+    Request.findByIdAndUpdate(req.params.id, {req_file: result.secure_url}, function(err){
+      if (err){
+        res.redirect('/adminhome');
+      } else {
+        res.redirect('/requests');
+      }
+    });
   });
 });
 module.exports = router;
